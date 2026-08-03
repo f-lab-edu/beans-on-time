6d4d927fdb808ae0d8321e71169909aabb345e8c
bluetoya <hjahn934@gmail.com>
2026-08-04 03:41:47 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/bluetoya/beansontime/BeansOnTimeApplication.java
@@ -0,0 +1,13 @@
+package com.bluetoya.beansontime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeansOnTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeansOnTimeApplication.class, args);
+    }
+
+}
