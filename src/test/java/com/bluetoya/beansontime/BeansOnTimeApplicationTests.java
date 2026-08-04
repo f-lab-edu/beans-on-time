f6d02b83704c7f74572ad8c5757f6b05cb68f863
bluetoya <hjahn934@gmail.com>
2026-08-05 02:02:21 +0900

    chore: MySQL, Flyway, TestContainers 설정

--- src/test/java/com/bluetoya/beansontime/BeansOnTimeApplicationTests.java
+++ src/test/java/com/bluetoya/beansontime/BeansOnTimeApplicationTests.java
@@ -2,9 +2,23 @@ package com.bluetoya.beansontime;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class BeansOnTimeApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer mysql =
+            new MySQLContainer(DockerImageName.parse("mysql:8.0.39"))
+                    .withDatabaseName("beans-on-time")
+                    .withUsername("user")
+                    .withPassword("password");
 
     @Test
     void contextLoads() {
